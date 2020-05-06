aeacb68c03c2b31c5124409618664f65d45c05ef
BrianCarducci <briancarducci@gmail.com>
2020-05-05 23:22:19 -0400

    testing cicd

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2>Login with Instagram</h2>
+        <h2>Login with Instagramsdf</h2>
         <button v-on:click="instagramAuthorize">Go!</button>
     </div>
 </template>
